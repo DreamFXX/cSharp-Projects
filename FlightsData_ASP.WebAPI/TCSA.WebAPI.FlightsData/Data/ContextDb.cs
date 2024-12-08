0e9bcc7baed247f01b66693db98d635ac6c47c4c
DreamFXX <filipek.husak@gmail.com>
2024-12-08 04:20:13 +0100

    added flights data proj

---
+++ FlightsData_ASP.WebAPI/TCSA.WebAPI.FlightsData/Data/ContextDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TCSA.WebAPI.FlightsData.Models;
+
+namespace TCSA.WebAPI.FlightsData.Data;
+public class FlightsDbContext2 : DbContext
+{
+    public FlightsDbContext2(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Flight> Flights { get; set; }
+}
