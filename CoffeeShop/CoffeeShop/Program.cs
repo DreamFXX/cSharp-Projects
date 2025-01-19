30f3f5b533d06a0b48974f04bb4af1b95c354def
DreamFXX <filipek.husak@gmail.com>
2025-01-19 15:54:01 +0100

    Coffee shop init

---
+++ CoffeeShop/CoffeeShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+configuration.GetConnectionString("DefaultCnn");
+
+
