d8a418cabd8d6cfcc27c17b5d3b261789d1aee3f
DreamFXX <filipek.husak@gmail.com>
2025-01-22 07:29:18 +0100

    Coffee EF Course init proj.

---
+++ CoffeeShop_EFCoreCrashCourse/CoffeeShop_EFCoreCrashCourse/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿namespace CoffeeShop_EFCoreCrashCourse;
+
+public class ProductsController
+{
+    public static void AddProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void UpdateProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void DeleteProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void ViewAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void ViewProductById()
+    {
+        throw new NotImplementedException();
+    }
+}
